6543c32ea22375762e565ba605ada0286784017e
Khaanh <khanh13nguyen13@gmail.com>
2026-01-09 14:19:53 +0100

    WIP: save/plus function

--- js/index.js
+++ js/index.js
@@ -2,14 +2,21 @@ const counterInput = document.querySelector("#counterInput");
 const btnPlus = document.querySelector("#btnPlus");
 const btnMinus = document.querySelector("#btnMinus");
 const btnSave = document.querySelector("#btnSave");
-let totalRep = document.querySelector("#totalRep");
+let counterDisplay = document.querySelector("#counterDisplay");
 let counter = 0;
+let totalCounter = 0;
 
 btnPlus.addEventListener("click", () => {
 	counter++;
 	counterInput.value = counter;
+
+	console.info(totalCounter, typeof totalCounter);
 });
 
 btnSave.addEventListener("click", () => {
-	totalRep.textContent = counter;
+	totalCounter = counter ? counter : counterInput.value;
+	counterDisplay.textContent = totalCounter;
+
+	counter = 0;
+	counterInput.value = "";
 });
