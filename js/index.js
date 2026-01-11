6227eb5839cf9048b65f4df18e09bc92382b4fa6
Khaanh <khanh13nguyen13@gmail.com>
2026-01-11 01:44:03 +0100

    feat: implement localStorage read/write functions

--- js/index.js
+++ js/index.js
@@ -2,29 +2,49 @@ const counterInput = document.querySelector("#counterInput");
 const btnPlus = document.querySelector("#btnPlus");
 const btnMinus = document.querySelector("#btnMinus");
 const btnSave = document.querySelector("#btnSave");
+const btnClear = document.querySelector(".clear");
+
 let counterDisplay = document.querySelector("#counterDisplay");
 let counter = 0;
 let totalCounter = 0;
 
-counterInput.focus();
+document.addEventListener("DOMContentLoaded", () => {
+	counterDisplay.textContent = totalCounter = localStorage.getItem(
+		"totalCounter"
+	)
+		? localStorage.getItem("totalCounter")
+		: 0;
+});
 
 btnPlus.addEventListener("click", () => {
 	totalCounter++;
+	localStorage.setItem("totalCounter", Number(totalCounter));
 	counterDisplay.textContent = totalCounter;
+
+	console.log(typeof localStorage.getItem("totalCounter"));
 });
 
 btnMinus.addEventListener("click", () => {
 	if (!totalCounter) return;
 
 	totalCounter--;
+	localStorage.setItem("totalCounter", Number(totalCounter));
 	counterDisplay.textContent = totalCounter;
 });
 
 btnSave.addEventListener("click", () => {
+	if (!counterInput.value) return;
+
 	totalCounter = counterInput.value
 		? totalCounter + Number(counterInput.value)
 		: totalCounter + Number(counterInput.value);
 	counterDisplay.textContent = totalCounter;
 
+	localStorage.setItem("totalCounter", Number(totalCounter));
+	counterInput.focus();
 	counterInput.value = "";
+});
+
+btnClear.addEventListener("click", () => {
+	localStorage.clear();
 });
