797c1ab2bb5633ecdb7bac9a113b5b318e44461b
Khaanh <khanh13nguyen13@gmail.com>
2026-04-27 00:47:31 +0200

    feat: add id to form

--- client/index.js
+++ client/index.js
@@ -3,9 +3,37 @@ import "/index.scss";
 
 const uid = new ShortUniqueId({ length: 13 });
 const btnClear = document.querySelector(".clear");
-const btnToggleWidget = document.querySelector("#btnToggleWidget");
+let generatedId, savedId, form, target, savedCounter;
+let counter = 0;
 
 btnClear.addEventListener("click", function () {
 	localStorage.clear();
 	location.reload();
 });
+
+document.addEventListener("click", (e) => {
+	form = e.target.closest("[data-action='formCount']");
+	target = e.target.closest("[data-action]");
+
+	console.log(target.dataset.action);
+
+	if (target.dataset.action === "increaseCount") increaseCounter(counter);
+	if (target.dataset.action === "decreaseCount") decreaseCounter(counter);
+	if (target.dataset.action === "saveCount") saveCounter(counter);
+
+	generatedId = uid.rnd();
+	form.id || form.setAttribute("id", generatedId);
+	localStorage.setItem("formID", form.id);
+});
+
+const increaseCounter = function (counter) {
+	counter++;
+	console.log(counter);
+};
+
+const decreaseCounter = function (counter) {
+	counter--;
+	console.log(counter);
+};
+
+const saveCounter = function (e) {};
